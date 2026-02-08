a2f1534a32dcd2215d8cea6d638eb6c1f1647b87
me9900-say <ideapaid911@gmail.com>
2026-02-08 20:42:42 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩ZAIDI-MD𓆪',
+  author: '‎',
+  botName: "𓆩ZAIDI-MD𓆪",
+  botOwner: 'ZAIDI', // Your name
+  ownerNumber: '923315462969', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/me9900-say/----",
+};
+
+module.exports = settings;
